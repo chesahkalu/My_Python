05920130458b29a1c61412fb18a56520519c38ec
Chesachi Victor Kalu <chesahkalu@gmail.com>
2023-01-14 23:46:20 +0100

    file 8 coded reviewed'

--- 8-random_list.py
+++ 8-random_list.py
@@ -9,9 +9,25 @@ for i in range(5): #does the code under 5 times
 	randomlist.append(random.randrange(2,50)) #append adds to the list. It adds random number generated between 2-50 with random.range
 
 for i in randomlist:
-	print(i)
+	print(i, end=' , ')
+print()
+print(randomlist)
 
 print()
 
 for i in reversed(randomlist): #reverses random list
-	print(i)
+	print(i, end=' , ')
+print()
+print(reversed(randomlist))
+
+print()
+
+newrandomlist= [random.randrange(2,50) for x in range(5)]
+
+for i in newrandomlist:
+	print(i, end='')
+
+print(newrandomlist)
+
+
+ 
