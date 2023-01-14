8d07f2670cf072b5a37b94527f6d5398fbf62782
Chesachi Victor Kalu <chesahkalu@gmail.com>
2023-01-14 23:50:43 +0100

    file 8 coded reviewed'

--- 8-random_list.py
+++ 8-random_list.py
@@ -17,16 +17,14 @@ print()
 
 for i in reversed(randomlist): #reverses random list
 	print(i, end=' , ')
-print()
-print(reversed(randomlist))
 
 print()
 
-newrandomlist= [random.randrange(2,50) for x in range(5)]
+newrandomlist= [random.randrange(2,50) for x in range(5)]#a concise way of creating a list(list comprehension)
 
 for i in newrandomlist:
-	print(i, end='')
-
+	print(i, end=' , ')
+print()
 print(newrandomlist)
 
 
