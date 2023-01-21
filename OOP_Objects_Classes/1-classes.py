69b06962654d89c6e0d690fde289e33c753825c8
Chesachi Victor Kalu <chesahkalu@gmail.com>
2023-01-21 23:42:47 +0100

    classes

--- OOP_Objects_Classes/1-classes.py
+++ OOP_Objects_Classes/1-classes.py
@@ -4,3 +4,8 @@
 #This classes can be uploaded with so many much datas and functions, and the functions and do so many powerful things with the datas.
 #This is the heartline of programming. 
 #Also,there are various class features that will help in making classes a more powerful tool in data manipulations, efficacy and usage.
+
+class Users: #A class of users is created, with the class title ALWAYS STARTING WITH CAPITAL LETTERS, and most have a line below(use `pass` if no code yet)
+    """This is a class of users, with this we will slowly creat a basic,
+    but proper class of users with various attributesand methods.
+    We will also try to perform multiple features on this classes to see the """#this a called Docstrings, a form of very important documentations about your classes.
