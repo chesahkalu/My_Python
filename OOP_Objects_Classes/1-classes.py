0f591ef41ad3c5afae1a546c19b6187ded83447b
Chesachi Victor Kalu <chesahkalu@gmail.com>
2023-01-21 23:51:14 +0100

    classes

--- OOP_Objects_Classes/1-classes.py
+++ OOP_Objects_Classes/1-classes.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
 
 #classes could be seen as a holder of objects(instances) with related datas(attributes) and functions(methods).
-#This classes can be uploaded with so many much datas and functions, and the functions and do so many powerful things with the datas.
+#This classes can be uploaded with so many much datas and functions, and the functions can do so many powerful things with the datas.
 #This is the heartline of programming. 
 #Also,there are various class features that will help in making classes a more powerful tool in data manipulations, efficacy and usage.
 
 class Users: #A class of users is created, with the class title ALWAYS STARTING WITH CAPITAL LETTERS, and most have a line below(use `pass` if no code yet)
-    """This is a class of users, with this we will slowly creat a basic,
-    but proper class of users with various attributesand methods.
-    We will also try to perform multiple features on this classes to see the """#this a called Docstrings, a form of very important documentations about your classes.
+    """This is a class of users, with this we will slowly create a basic,
+    but proper class of users with various attributes and methods.
+    We will also try to perform multiple features on this classes to see the """#this is called Docstrings, a form of very important documentations about your classes.
+
+    name = ''
+    age = 0
+    id_no = ''
+    height = 0
+    """This are the class attributes that will be assinged to objects.
+    More attributes can be added to objects, in various ways. Even while creating the objects.
+    The attribute values are left empty so that creating objects are still possible without assigning 
+    values w=to them already"""
+
+    
+
