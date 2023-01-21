a7a8532429e1908571197b514e13ff61db40343f
Chesachi Victor Kalu <chesahkalu@gmail.com>
2023-01-21 23:32:58 +0100

    classes files added with intro comment

---
+++ OOP_Objects_Classes/1-classes.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+#classes could be seen as a holder of objects(instances) with related datas(attributes) and functions(methods).
+#This classes can be uploaded with so many much datas and functions, and the functions and do so many powerful things with the datas.
+#This is the heartline of programming. 
+#Also,there are various class features that will help in making classes a more powerful tool in data manipulations, efficacy and usage.
