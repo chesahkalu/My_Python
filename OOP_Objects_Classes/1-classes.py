3ec737a4670534740050c8a2e2ee68fa35c82462
Chesachi Victor Kalu <chesahkalu@gmail.com>
2023-01-22 02:26:11 +0100

    Classes

--- OOP_Objects_Classes/1-classes.py
+++ OOP_Objects_Classes/1-classes.py
@@ -18,25 +18,36 @@ class Users: #A class of users is created, with the class title ALWAYS STARTING 
     More attributes can be added to objects, in various ways. Even while creating the objects.
     The attribute values are left empty so that creating objects are still possible without assigning 
     values w=to them already"""
+    
+        
 
     def say_who(self):
-        print(f'I am {self.name} and i am {self.age} years old')
+        print(f'I am {self.name},I am {self.age} years old and my height is {self.height}cm')
     """This is a function(Method), the (self) is used to make refrence to the object using the function,
     when an object calls the function, it uses the attribute data assigned to the self of that object,
     It uses the variables of the objects it was run with."""
 
     def __init__(self, name, age, height):
+        self.name = name #the values in the function arguments are stored to the fields in the attribute(of the object when created )
+        self.age = age
+        self.height = height
+    """The init function. This funtion is used to initialize attribute to object during creation of objects
+    self is used to refrence the new object being created."""
 
 
 
 user1 = Users() #now an object is created, by giving it a name and telling the program it is an instance of the class
 
 user1.name = 'Andrew'
-user1.age = 20 #objects are assigned some attributes, which are now the objects fields. With their DATA values.
+user1.age = 20
+user1.height = 165 #objects are assigned some attributes, which are now the objects fields. With their DATA values.
+
+user2 = Users("Banarbas", 23, 180) #a second object is created and with the init function the attributes are asigned and initialized to the object during creation.
 
 print(user1.name, '&' , user1.age)
 
 user1.say_who()#a method is assigned to an object in the class. Method is run and uses the arguments of the object because self was used in creating the arguments.
+user2.say_who()
 
 
 
