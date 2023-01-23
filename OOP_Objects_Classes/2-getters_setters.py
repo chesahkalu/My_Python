68991ba02abde034dfe0a8dfa055bcf584c54a97
Chesachi Victor Kalu <chesahkalu@gmail.com>
2023-01-23 23:43:32 +0100

    getters and setters

--- OOP_Objects_Classes/2-getters_setters.py
+++ OOP_Objects_Classes/2-getters_setters.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+"""in this code we are going to create a class of rectangle,encapsulate its datas attributes,
+make the data private(to avoid them from being accidentally modified by other methods and classes),
+then create getters(to retrieve the datas) and setters(to change the data). Also, the setters would
+function to raise type value errors if the data change is not in accordance to the desired requirement."""
+
+class Rectangles:
+    def __init__(self, length=0, width=0):
+        self.length= length
+        self.width= width
+        """A class is created, initialization of attributes set for during object creation.
+        Remember the use of self, to refer to the object being used during initialization"""
+
