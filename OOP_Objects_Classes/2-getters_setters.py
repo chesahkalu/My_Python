464a4fb0ea8b56f0f3887f4613e22d35054129f0
Chesachi Victor Kalu <chesahkalu@gmail.com>
2023-01-29 23:55:36 +0100

    codes added

--- OOP_Objects_Classes/2-getters_setters.py
+++ OOP_Objects_Classes/2-getters_setters.py
@@ -75,4 +75,9 @@ class Rectangles:
                 rect.append("\n")
         return ("".join(rect))
 
+    def __repr__(self):
+        """Return the string representation of the Rectangle."""
+        rect = "Rectangle(" + str(self.__width)
+        rect += ", " + str(self.__height) + ")"
+        return (rect)
         
