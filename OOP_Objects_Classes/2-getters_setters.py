248fa862c6a546808fa2f78e0003ebe476e35742
Chesachi Victor Kalu <chesahkalu@gmail.com>
2023-01-29 19:35:41 +0100

    code updates

--- OOP_Objects_Classes/2-getters_setters.py
+++ OOP_Objects_Classes/2-getters_setters.py
@@ -7,30 +7,30 @@ function to raise type value errors if the data change is not in accordance to t
 We are going to use the datas to to create some methods and perform some funtions"""
 
 class Rectangles:
-    def __init__(self, length=0, width=0):
-        self.length= length
+    def __init__(self, height=0, width=0):
+        self.height= height
         self.width= width
         """A class is created, initialization of attributes set for during object creation.
         Remember the use of self, to refer to the object being used during initialization"""
 
     @property #this is a function used to get the private attribute data of the refered object(property).it creates and return the property of the object
-    def length(self):
-        return self.__length
+    def height(self):
+        return self.__height
         """this is the getter function, that gets the length value and the two underscores return them as private data
         therefore returning __length"""
 
-    @length.setter #the function to change the length attribute while raising some errors if the change isnt in a required form
-    def length(self, value):
-        """length is set, and value is created to assing to length"""
+    @height.setter #the function to change the length attribute while raising some errors if the change isnt in a required form
+    def height(self, value):
+        """length is set, and value is created to assign to length"""
 
         if type(value) is not int:
-            raise TypeError("size must be an integer")
+            raise TypeError("height must be an integer")
         elif value < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError("height must be >= 0")
             """value are given conditions to meet when being set, else errors are raise, if no conditions are required,
             then self's length is just assigned as the data assigned to value, hence data is changed and set"""
         else:
-            self._length = value
+            self.__height = value
             """Now the only way to change the private data is with the set function. Using the inittialization function at object creation
             will cause an error"""
 
@@ -43,16 +43,27 @@ class Rectangles:
     @width.setter
     def width(self, value):
         if type(value) is not int:
-            raise TypeError("size must be an integer")
+            raise TypeError("width must be an integer")
         elif value < 0:
-            raise ValueError("size must be a positive number")
+            raise ValueError("width must be >= 0")
         else:
             self.__width = value
 
 #classe created. Attributes created and encapsulated with getters and setters.Now is time to create some objects and methods.
 
 
+    def area(self):
+        a = self.__height * self.__width
+        return a
 
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return self.__height + self.__width + self.__height + self.__width
 
-
-
+rec1 = Rectangles(8,5)
+rec2 = Rectangles(8,0)
+print(rec1.area())
+print(rec1.perimeter())
+print(rec2.area())
+print(rec2.perimeter())
