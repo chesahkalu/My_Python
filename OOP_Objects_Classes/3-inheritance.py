259c57050241c0768af0262389660e5180036421
Chesachi Victor Kalu <chesahkalu@gmail.com>
2023-02-04 15:02:42 +0100

    Inheritance added

---
+++ OOP_Objects_Classes/3-inheritance.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""class inheritance , simply acquires all the methods and attributes of a class, making it a subclass
+and the copied class a super class"""
+
+"""The subclass can aswell be adjusted in different ways, the init
+functions of the super class has to be called in the sub class as well as the sub class having its 
+own init functions for its attribute."""
+
+"""Methods can be overiden by siply Defining them and rewriting the code in the sub class.
+Methods can alsobe referred to by using the super() function to call on them."""
+
