281272a63c07e6abe4021fc74ee13d68a70d850c
Chesachi Victor Kalu <chesahkalu@gmail.com>
2023-02-04 23:37:15 +0100

    code update

--- OOP_Objects_Classes/3-inheritance.py
+++ OOP_Objects_Classes/3-inheritance.py
@@ -10,3 +10,16 @@ own init functions for its attribute."""
 """Methods can be overiden by siply Defining them and rewriting the code in the sub class.
 Methods can alsobe referred to by using the super() function to call on them."""
 
+class Polygon:
+    def __init__(self, sides):
+        self.sides = sides #this would a list of differnt int reping the sized of diff side of the polygon
+    
+    def displayinfo(self):
+        print("A polygon is a two-dimensional geometric figure that has a finite number of sides,\
+              The sides of a polygon are made of straight line segments connected to each other end to end.\
+              Thus, the line segments of a polygon are called sides or edges.")
+        
+    def get_perimeter(self):
+        perimeter = sum(self.sides)
+
+class Rectangle(Polygon):
