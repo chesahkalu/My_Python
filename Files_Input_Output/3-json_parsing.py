c26cdfc3b9cbf36c21e630eea8e08934e74555ff
Chesachi Victor Kalu <chesahkalu@gmail.com>
2023-02-06 19:11:48 +0100

    Json codes

---
+++ Files_Input_Output/3-json_parsing.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+JSON (JavaScript Object Notation) is a popular data format used for representing structured data. 
+It's common to transmit and receive data between a server and web application in JSON format. 
+Code to parse, read and write JSON in Python. Also, converting JSON to dict and pretty print it."""
+
+"""
+normally in python json existas as string
+also it can exist as a file, and to work and parse or print or convert from file to string, 
+we import the json moule"""
+
