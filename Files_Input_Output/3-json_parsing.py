9045e934cfbee415fbc06bd86e319d635ec4c51a
Chesachi Victor Kalu <chesahkalu@gmail.com>
2023-02-06 19:12:36 +0100

    Jsos codes

--- Files_Input_Output/3-json_parsing.py
+++ Files_Input_Output/3-json_parsing.py
@@ -10,3 +10,21 @@ normally in python json existas as string
 also it can exist as a file, and to work and parse or print or convert from file to string, 
 we import the json moule"""
 
+import json
+
+person = '{"name": "Bob", "languages": ["English", "French"]}' #json string
+person_dict = json.loads(person) #this parses the string to return a dictionary
+print( person_dict) #prints dictionary of person
+
+with open('path_to_file/person.json', 'r') as f: #this opens a file containing json string(object)
+  data = json.load(f) #this parses the file into a dictionary
+print(data) #prints dictionary of person
+
+"""note ~loads~ is used toparse the json string while ~load~ is used to parse the file"""
+
+
+
+
+
+
+
