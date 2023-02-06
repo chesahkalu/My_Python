eaf42a7e96e080cd4d44f90a2b4b4cb4b2c45f6a
Chesachi Victor Kalu <chesahkalu@gmail.com>
2023-02-06 19:13:49 +0100

    Json codes

--- Files_Input_Output/3-json_parsing.py
+++ Files_Input_Output/3-json_parsing.py
@@ -35,3 +35,24 @@ with open('person.txt', 'w') as json_file: #this opens a file in writing mode
   json.dump(person_dict, json_file) #this writes the dictionary into a file called son file in json string mode
 
 
+"""
+To analyze and debug JSON data, we may need to print it in a more readable format. 
+This can be done by passing additional parameters indent and sort_keys to json.dumps() and json.dump() method.
+This is pretty print Json
+"""
+
+import json
+
+person_string = '{"name": "Bob", "languages": "English", "numbers": [2, 1.6, null]}'
+
+# Getting dictionary
+person_dict = json.loads(person_string)
+
+# Pretty Printing JSON string back
+print(json.dumps(person_dict, indent = 4, sort_keys=True))
+
+"""
+In the above program, we have used 4 spaces for indentation. And, the keys are sorted in ascending order.
+
+By the way, the default value of indent is None. And, the default value of sort_keys is False."""
+
