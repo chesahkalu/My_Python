f4ce069ecc8c3559638aa146152766631697317b
Chesachi Victor Kalu <chesahkalu@gmail.com>
2023-02-09 20:04:27 +0100

    notes

--- Functions_Modules/class_import.py
+++ Functions_Modules/class_import.py
@@ -6,9 +6,16 @@ aswell as some of its methods and attributess manipulated from here"""
 
 r3 = Rectangle(10, 2, 0, 0, 12)
 print(r3.id)
+"""Init method initialized and id attribue for object r3 checked"""
+
 
 try:
     r = Rectangle(10, 2)
     r.width = -10
 except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
+"""class setters method validation check"""
+
+r3 = Rectangle(8, 7, 0, 0, 12)
+print(r3.area())
+"""area method applied and checked"""
