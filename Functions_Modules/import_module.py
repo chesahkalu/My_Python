36d0e457deb85d05e96aaa0e28c333465fd73cfd
Chesachi Victor Kalu <chesahkalu@gmail.com>
2023-02-09 16:42:24 +0100

    notes and codes fixed

--- Functions_Modules/import_module.py
+++ Functions_Modules/import_module.py
@@ -5,6 +5,11 @@
 import some_functions # this imports the file that has all the written functions. tthis can be written like below to use a simplier name
 from some_functions import interest_rate # this imports only one function from the file module
 from some_functions import cipher as ex # this imports function cipher from module some_functions and renamed to ex
+adder = __import__("some_functions").adder
+
+"""
+we import using the name of the file. Naming a module is crucial. If not named rightly(with "-", or "number")
+would cause an error during importation, as it wont be a recognized module"""
 
 print(some_functions.add_int(3,8))
 
