1b34b7fab6fe6eaa52ef38bf7e7350072e23b967
Chesachi Victor Kalu <chesahkalu@gmail.com>
2023-02-10 20:31:49 +0100

    updates

--- Files_Input_Output/3-json_parsing.py
+++ Files_Input_Output/3-json_parsing.py
@@ -14,13 +14,13 @@ import json
 
 person = '{"name": "Bob", "languages": ["English", "French"]}' #json string
 person_dict = json.loads(person) #this parses the string to return a dictionary
-print( person_dict) #prints dictionary of person
+print(person_dict) #prints dictionary of person
 
 with open('path_to_file/person.json', 'r') as f: #this opens a file containing json string(object)
   data = json.load(f) #this parses the file into a dictionary
 print(data) #prints dictionary of person
 
-"""note ~loads~ is used toparse the json string while ~load~ is used to parse the file"""
+"""note ~loads~ is used to parse the json string while ~load~ is used to parse the file"""
 
 #this whole function can be done in the opposite direction>>>>>>
 
@@ -31,8 +31,8 @@ print(person_json) #prints a json string
 
 person_dict = {"name": "Bob", "languages": ["English", "French"], "married": True, "age": 32} # a dictionary
 
-with open('person.txt', 'w') as json_file: #this opens a file in writing mode
-  json.dump(person_dict, json_file) #this writes the dictionary into a file called json_file in json string mode
+with open('person.json', 'w') as json_file: #this opens a file in writing mode
+  json.dump(person_dict, json_file) #this writes the dictionary into a file called person.json in json string mode
 
 
 """
