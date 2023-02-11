b8cb8b44fedce9ee8e19028a89df284b2485ee06
Chesachi Victor Kalu <chesahkalu@gmail.com>
2023-02-11 15:14:40 +0100

    comments

--- Functions_Modules/import_module.py
+++ Functions_Modules/import_module.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 
+"""We can import all there is in another file module, including variable, functions and class into another module
+Just import the file name, and access the object with a dot(.) notation after the object"""
 
 
 import some_functions # this imports the file that has all the written functions. this can be written like below to use a simplier name
 import some_functions as sm
-from some_functions import interest_rate # this imports only one function from the file module
+from some_functions import interest_rate # this imports only one function from the file module 
 from some_functions import cipher as ex # this imports function cipher from module some_functions and renamed to ex
 ad = __import__("some_functions").adder # this is another way of importing a function from a module, ostly used when module name is conventional,imports adder ass ad.
 from some_functions import intro # this is how to import from a different directory 
+import Functions_Modules.some_functions 
 
 """
 we import using the name of the file. Naming a module is crucial. If not named rightly(with "-", or "number")
