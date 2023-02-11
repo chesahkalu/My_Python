c24a2d35bb72491bd397b1d69328f6327b267213
Chesachi Victor Kalu <chesahkalu@gmail.com>
2023-02-11 01:46:59 +0100

    csv codes and notes

--- Files_Input_Output/4-csv.py
+++ Files_Input_Output/4-csv.py
@@ -56,4 +56,13 @@ with open("people.csv", 'r') as file:
     for row in csv_file:
         print(dict(row)) #the dict() method used to create dictionaries inside the for loop.
 
-"""to write """
+"""to write dictionaries into csv files"""
+
+with open('players.csv', 'w', newline='') as file:
+    fieldnames = ['player_name', 'fide_rating']
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
+
+    writer.writeheader()
+    writer.writerow({'player_name': 'Magnus Carlsen', 'fide_rating': 2870})
+    writer.writerow({'player_name': 'Fabiano Caruana', 'fide_rating': 2822})
+    writer.writerow({'player_name': 'Ding Liren', 'fide_rating': 2801})
