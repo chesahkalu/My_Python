3eb0ef683492d9027c026c08dca710f88d29de5f
Chesachi Victor Kalu <chesahkalu@gmail.com>
2023-02-11 01:01:38 +0100

    notes added

--- Files_Input_Output/4-csv.py
+++ Files_Input_Output/4-csv.py
@@ -12,6 +12,7 @@ To use CSV files, import csv
 
 import csv
 
+"""to read a csv file with non default deimiter, the delimeter must be indicated"""
 
 with open('people.csv', 'r') as file: #opens a file name "people.csv", in read mode.
     reader = csv.reader(file) #csv file read like this, and returns an object assigned to 'reader'. if delimeter = -, then(csv.read(file, delimeter='\t'))
@@ -20,10 +21,21 @@ with open('people.csv', 'r') as file: #opens a file name "people.csv", in read m
 
 
 """to write csv file, a function first returns a string object in a tabular form with delimeter and the object can
-tthen be written into a csv file"""
+then be written into a csv file"""
 
 with open('protagonist.csv', 'w', newline='') as file: #file created and opened in a write mode
-    writer = csv.writer(file) #write assigned the object
-    writer.writerow(["SN", "Movie", "Protagonist"])
+    writer = csv.writer(file) #writer assigned the object that would be written
+    writer.writerow(["SN", "Movie", "Protagonist"]) #rows are written accordinly
     writer.writerow([1, "Lord of the Rings", "Frodo Baggins"])
     writer.writerow([2, "Harry Potter", "Harry Potter"])
+
+
+"""A two dimensional list can be written into a csv file, each list index taking a row number accordinly"""
+
+csv_rowlist = [["SN", "Movie", "Protagonist"], [1, "Lord of the Rings", "Frodo Baggins"],
+               [2, "Harry Potter", "Harry Potter"]]
+with open('protagonist.csv', 'w') as file:
+    writer = csv.writer(file)
+    writer.writerows(csv_rowlist)
+    
+"""writing a """
