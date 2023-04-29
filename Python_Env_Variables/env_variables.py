c6b0c7c2d5ddd37d203ebfa0444f638c7fed4803
chesahkalu <chesahkalu@gmail.com>
2023-04-29 16:07:50 +0100

    env

---
+++ Python_Env_Variables/env_variables.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+import os
+
+"""this syntax is used to get the value of environment variables already set in the system"""
+
+user = os.getenv('USER') # get the value of environment variable 'USER' which is already set in the system
+print(user) # print the value of environment variable 'USER'
+
+"""if the environment variable 'USER' is not set in the system, then the value of user will return None"""
+user = os.getenv('USER' , 'No user') # this will return 'No user' if the environment variable 'USER' is not set in the system
+
+
+
+user = os.environ['USER'] # this is another method of getting the value of environment variable 'USER', but this will raise an exception if the environment variable 'USER' is not set in the system
+user = os.environ.get['USER'] # this will return None if the environment variable 'USER' is not set in the system
+print(user)
+
+
+"""To set the value of environment variable 'USER' in the system, we can use the following syntax
+
+On your terminal, type the following command: "export database=mysql" and press enter. This will create an
+environment variable 'database' with value 'mysql' in the system if it doesn't already exist.
+
+This environment variable will be available to all the programs running in the current shell. If you open a new
+shell, the environment variable 'database' will not be available to the programs running in the new shell.
+When you close the current shell, the environment variable 'database' will be deleted from the system."""
+
+
+"""To set the value of environment variable 'USER' in the system permanently, we can use the following syntax
+
+A .env file can be used to store the value of environment variables permanently in the system. The .env file
+will be automatically loaded when the system starts up. The .env file should be placed in the home directory
+of the user. The .env file should be in the following format: database=mysql ,  one environment variable per line."""
+
+"""This file can be included inthe gitignore file so that it is not pushed to the remote repository as it contains
+private information. An .env.example file can be created which will contain the names of the environment variables"""
+
